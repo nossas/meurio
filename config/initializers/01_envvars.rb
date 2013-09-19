198e431fbf0c97c409b2957e0ed92dab43681958
Nícolas Iensen <nicolas@iensen.me>
2013-09-19 11:52:15 -0300

    [#56227138] change PdP host variable name

--- config/initializers/01_envvars.rb
+++ config/initializers/01_envvars.rb
@@ -1,2 +1,2 @@
-ENV["PANELADEPRESSAO_HOST"] = "http://paneladepressao-staging.herokuapp.com"
+ENV["PDP_HOST"] = "http://paneladepressao-staging.herokuapp.com"
 ENV["IMAGINE_HOST"] = "http://imagine-staging.herokuapp.com"
