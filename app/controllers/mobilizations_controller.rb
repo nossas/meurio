90f82e0f06bce8ba0b30446107e28aa9ecf391e9
Josias Schneider <josias.ds@gmail.com>
2013-09-23 18:47:16 -0300

    [#56227386] get tweets by the mobilization's hashtag

--- app/controllers/mobilizations_controller.rb
+++ app/controllers/mobilizations_controller.rb
@@ -1,4 +1,4 @@
 class MobilizationsController < InheritedResources::Base
   before_filter(only: :show) { @facts = Fact.where(mobilization_id: params[:id]).order("created_at DESC") }
-  before_filter(only: :show) { @tweets = Tweet.where(hashtag: 'DeGuardaNoBondinho').order("published_at DESC") }
+  before_filter(only: :show) { @tweets = Tweet.where(hashtag: Mobilization.find(params[:id]).hashtag).order("published_at DESC") }
 end
