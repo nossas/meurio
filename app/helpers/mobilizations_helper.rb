55dd2fe0c1cc175a306e0caaaf76b75293dfee15
Nícolas Iensen <nicolas@iensen.me>
2013-09-25 16:00:25 -0300

    [#57075112] remove MobilizationsHelper::link_twitter_user method

--- app/helpers/mobilizations_helper.rb
+++ app/helpers/mobilizations_helper.rb
@@ -1,9 +1,4 @@
 module MobilizationsHelper
-
-  def link_twitter_user username
-    link_to "@#{username}", "http://twitter.com/#{username}", target: :blank
-  end
-
   def user_link comment
     if comment.tweet?
       "http://twitter.com/#{comment.username}"
