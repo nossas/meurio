e09a19e9881c9d44f3bfa58c39d98766605411ed
Nícolas Iensen <nicolas@iensen.me>
2013-09-27 10:39:16 -0300

    [#56227496] add feature share_a_mobilization_on_twitter.feature

--- features/support/helpers.rb
+++ features/support/helpers.rb
@@ -22,6 +22,7 @@ def to_element string
   return ".comments .empty"                             if string == "this mobilization have no comment yet"
   return ".facebook_posts .text"                        if string == "this mobilization Facebook post"
   return ".mobilization form"                           if string == "the mobilization form"
+  return ".mobilization .share .twitter-share-button"   if string == "the Twitter share button of this mobilization"
 end
 
 def to_text string
