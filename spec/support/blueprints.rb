b3536fd3fe04c933cb735d6e7e7b0605912ea5e1
Josias Schneider <josias.ds@gmail.com>
2013-10-10 19:08:07 -0300

    [#56226944] add user confirmed_at to blueprint; add default path for sign in

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -79,8 +79,9 @@ Clipping.blueprint do
 end
 
 User.blueprint do
-  first_name  { Faker::Name.first_name }
-  last_name   { Faker::Name.last_name }
-  email       { Faker::Internet.email }
-  password    { Faker::Internet.password }
+  first_name   { Faker::Name.first_name }
+  last_name    { Faker::Name.last_name }
+  email        { Faker::Internet.email }
+  password     { Faker::Internet.password }
+  confirmed_at { Time.now }
 end
