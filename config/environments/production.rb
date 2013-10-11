f95c623fc72bfa03243f3f328e29aaa324d2c6bf
Josias Schneider <josias.ds@gmail.com>
2013-10-11 15:05:45 -0300

    [#56226936] configure sendgrid; change devise translation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,6 @@ Meurio::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'meurio.herokuapp.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
@@ -78,4 +77,16 @@ Meurio::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  default_url_options[:host] = ENV["HOST"]
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password => ENV["SENDGRID_PASSWORD"],
+    :domain => ENV["HOST"],
+    :address => "smtp.sendgrid.net",
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
