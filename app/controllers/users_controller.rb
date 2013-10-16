6b436c9ef4f1e7a2a2ae7d9abdd5ce2afc8ccd05
Josias Schneider <josias.ds@gmail.com>
2013-10-16 17:35:30 -0300

    [#56226998] fix profile editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < InheritedResources::Base
-  before_action(only: :edit) { @user = current_user }
+  before_action(only: [:edit, :update]) { @user = current_user }
   before_action :authenticate_user!, except: :index
 
   layout false, only: :index
+
+  def update
+    update! notice: 'Perfil atualizado!'
+  end
+
+  def permitted_params
+    {:user => params.require(:user).permit(:first_name, :last_name)}
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :email, :password, :first_name, :last_name, presence: true
+  validates :email, :first_name, :last_name, presence: true
   validates :email, format: { with: /([0-9a-zA-Z]+[-._+&amp;])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}/ }
+  validates :password, on: :create, presence: true, length: { minimum: 6, maximum: 128 }
+  validates :password, on: :update, allow_blank: true, length: { minimum: 6, maximum: 128 }
   
   mount_uploader :image, UserUploader
 
