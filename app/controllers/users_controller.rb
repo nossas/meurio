c70df23c4a1e1fa2c7f47cdf79390a60a5244cff
Josias Schneider <josias.ds@gmail.com>
2013-10-18 16:36:35 -0300

    [#56226998] add email field to profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,6 @@ class UsersController < InheritedResources::Base
   end
 
   def permitted_params
-    {:user => params.require(:user).permit(:image, :first_name, :last_name, :bio, :birthdate, :profession, :home_postcode, :work_postcode, :phone, :secondary_email, :gender, :public, :home_address_street, :home_address_number, :home_address_extra, :home_address_district, :home_city, :home_state, :work_address_street, :work_address_number, :work_address_extra, :work_address_district, :work_city, :work_state)}
+    {:user => params.require(:user).permit(:image, :first_name, :last_name, :email, :bio, :birthdate, :profession, :home_postcode, :work_postcode, :phone, :secondary_email, :gender, :public, :home_address_street, :home_address_number, :home_address_extra, :home_address_district, :home_city, :home_state, :work_address_street, :work_address_number, :work_address_extra, :work_address_district, :work_city, :work_state)}
   end
 end
