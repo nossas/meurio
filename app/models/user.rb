2d2c1c503045136180c1e4ec8f77aeb096a4d140
Nícolas Iensen <nicolas@iensen.me>
2013-10-21 14:16:56 -0200

    [#56226998] add validation to secondary email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :email, :first_name, :last_name, presence: true
-  validates :email, format: { with: /([0-9a-zA-Z]+[-._+&amp;])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}/ }
+  validates :email, :secondary_email, format: { with: /([0-9a-zA-Z]+[-._+&amp;])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}/ }
   validates :password, on: :create, presence: true, length: { minimum: 6, maximum: 128 }
   validates :password, on: :update, allow_blank: true, length: { minimum: 6, maximum: 128 }
   validates :phone, format: { with: /\([\d]{2}\)\s[\d]{8,9}/ }, allow_blank: true
