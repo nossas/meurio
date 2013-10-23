c2375acaa13f9fb004273568ebb0dcdf990bc021
Josias Schneider <josias.ds@gmail.com>
2013-10-23 11:39:49 -0200

    [#56226936] change mail sender

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'josias.ds@gmail.com'
+  config.mailer_sender = 'contato@meurio.org.br'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
