1cd2d31cffd1b9b0fb7b394277dac4791927d104
Josias Schneider <josias.ds@gmail.com>
2013-10-23 11:42:16 -0200

    [#56226936] redirect to edit profile after email confirmation

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -3,7 +3,7 @@ class ConfirmationsController < Devise::ConfirmationsController
   private
 
   def after_confirmation_path_for(resource_name, resource)
-    root_path
+    edit_profile_path
   end
 
 end
