859a7603017523db9a25920808bf09d9d411593e
Josias Schneider <josias.ds@gmail.com>
2013-10-24 15:03:08 -0200

    [#58812412] override devise passwords controller; add password recovery form

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,20 @@
+class PasswordsController < Devise::PasswordsController
+  layout false
+  
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+
+    if successfully_sent?(resource)
+      respond_with({}, :location => after_sending_reset_password_instructions_path_for(resource_name))
+    else
+      redirect_to root_path, alert: resource.errors.full_messages.first
+    end
+  end
+  
+  protected
+  
+  def after_sending_reset_password_instructions_path_for(resource_name)
+    root_path
+  end
+  
+end
