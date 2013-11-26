c152227829a0981aea05dfaf495f02abd01e9ee7
Nícolas Iensen <nicolas@iensen.me>
2013-11-26 10:26:42 -0200

    [#61407808] changed secret token and session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Meurio::Application.config.session_store :cookie_store, key: '_meurio_session'
+if Rails.env.production?
+  Meurio::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio.org.br'
+else
+  Meurio::Application.config.session_store :cookie_store, key: '_meurio_accounts_session'
+end
