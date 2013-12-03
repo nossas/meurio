7a527dda9f8c87abdbe1f872a5edc2acfbb05a62
Josias Schneider <josias.ds@gmail.com>
2013-12-03 17:36:36 -0200

    [#59260840] add Home call-to-action when logged in/out

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20131202230252) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "campaigns", force: true do |t|
     t.string   "name"
     t.string   "link"
