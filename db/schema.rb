b8f06c508ed045d7cbb82e713c9d71fa1ba41f0c
Nícolas Iensen <nicolas@engage.is>
2013-12-11 14:08:45 -0200

    Fix petitions image

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131204183309) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "campaigns", force: true do |t|
     t.string   "name"
     t.string   "link"
