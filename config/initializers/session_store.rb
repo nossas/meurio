5c32dcff8a4fe072e6c4cdbb72587ff3d6dc2684
Nícolas Iensen <nicolas@iensen.me>
2014-01-30 20:41:19 -0200

    Config SSI for staging

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.production?
-  Meurio::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio.org.br'
+  Meurio::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio.org.br', expire_after: 100.years
+elsif Rails.env.staging?
+  Meurio::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio-staging.org.br', expire_after: 100.years
 else
-  Meurio::Application.config.session_store :cookie_store, key: '_meurio_accounts_session'
+  Meurio::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', expire_after: 100.years
 end
