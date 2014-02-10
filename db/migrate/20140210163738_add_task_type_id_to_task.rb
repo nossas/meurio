32b34879aea3c0dce257efc382ae0cd9d1077500
Nícolas Iensen <nicolas@engage.is>
2014-02-10 20:09:15 -0200

    Fix migration add task type id to task

--- db/migrate/20140210163738_add_task_type_id_to_task.rb
+++ db/migrate/20140210163738_add_task_type_id_to_task.rb
@@ -1,9 +1,9 @@
 class AddTaskTypeIdToTask < ActiveRecord::Migration
   def up
-    "ALTER TABLE tasks ADD COLUMN task_type_id integer;"
+    execute "ALTER TABLE tasks ADD COLUMN task_type_id integer;"
   end
 
   def down
-    "ALTER TABLE tasks DROP COLUMN task_type_id;"
+    execute "ALTER TABLE tasks DROP COLUMN task_type_id;"
   end
 end
