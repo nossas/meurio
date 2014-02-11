d5655b4cefa38c910fe2e746f132df8fe0e16053
Nícolas Iensen <nicolas@engage.is>
2014-02-11 15:48:26 -0200

    Fix migration

--- db/migrate/20140210222216_add_id_to_task.rb
+++ db/migrate/20140210222216_add_id_to_task.rb
@@ -1,7 +1,11 @@
 class AddIdToTask < ActiveRecord::Migration
   def up
-    execute "CREATE SEQUENCE tasks_serial START 1"
-    execute "ALTER TABLE tasks ADD COLUMN id integer PRIMARY KEY DEFAULT nextval('tasks_serial')"
+    if Rails.env.production? || Rails.env.staging?
+      execute "ALTER TABLE tasks ADD COLUMN id integer;"
+    else
+      execute "CREATE SEQUENCE tasks_serial START 1;"
+      execute "ALTER TABLE tasks ADD COLUMN id integer PRIMARY KEY DEFAULT nextval('tasks_serial');"
+    end
   end
 
   def down
