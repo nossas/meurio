2061aabba8a0cf704005da0b7beba5cf924151e8
Nícolas Iensen <nicolas@engage.is>
2014-02-11 15:50:41 -0200

    Fix migration

--- db/migrate/20140210223105_add_id_to_task_subscription.rb
+++ db/migrate/20140210223105_add_id_to_task_subscription.rb
@@ -1,7 +1,11 @@
 class AddIdToTaskSubscription < ActiveRecord::Migration
   def up
-    execute "CREATE SEQUENCE task_subscriptions_serial START 1"
-    execute "ALTER TABLE task_subscriptions ADD COLUMN id integer PRIMARY KEY DEFAULT nextval('task_subscriptions_serial')"
+    if Rails.env.production? || Rails.env.staging?
+      execute "ALTER TABLE task_subscriptions ADD COLUMN id integer"
+    else
+      execute "CREATE SEQUENCE task_subscriptions_serial START 1"
+      execute "ALTER TABLE task_subscriptions ADD COLUMN id integer PRIMARY KEY DEFAULT nextval('task_subscriptions_serial')"
+    end
   end
 
   def down
