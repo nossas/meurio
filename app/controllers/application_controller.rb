a62b36898042c4a7f7145e523f8a0b7ff6359d82
Nícolas Iensen <nicolas@engage.is>
2014-02-18 09:19:04 -0300

    Add login via get for dev env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
 
+  before_filter { session[:ssi_user_id] = params[:sign_in] if Rails.env.development? && params[:sign_in] }
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to ENV["MEURIO_ACCOUNTS_URL"] + "?redirect_url=#{request.url}"
   end
