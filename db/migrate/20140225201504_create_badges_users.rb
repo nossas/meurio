05579bd3b9c7d1793f8d985bba8b746330d25f19
Josias Schneider <josias.ds@gmail.com>
2014-02-25 18:52:09 -0300

    [#65315292] fix create_badges_users migration

--- db/migrate/20140225201504_create_badges_users.rb
+++ db/migrate/20140225201504_create_badges_users.rb
@@ -2,7 +2,7 @@ class CreateBadgesUsers < ActiveRecord::Migration
   def change
     create_table :badges_users, id: false do |t|
       t.references :badge
-      t.references :user
+      t.integer :user_id
     end
     add_index :badges_users, [:badge_id, :user_id], unique: true
   end
