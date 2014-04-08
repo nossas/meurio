a6beb9fc5b67c5772ba6ea967248097c90acf542
Josias Schneider <josias.ds@gmail.com>
2014-04-08 11:50:07 -0300

    fix migration

--- db/migrate/20140408134845_re_add_server_multitude.rb
+++ db/migrate/20140408134845_re_add_server_multitude.rb
@@ -5,7 +5,6 @@ class ReAddServerMultitude < ActiveRecord::Migration
       raise "MULTITUDE_DBHOST is missing" if ENV["MULTITUDE_DBHOST"].nil?
       raise "MULTITUDE_DBUSER is missing" if ENV["MULTITUDE_DBUSER"].nil?
       raise "MULTITUDE_DBPASS is missing" if ENV["MULTITUDE_DBPASS"].nil?
-      execute "CREATE EXTENSION postgres_fdw;"
       execute "CREATE SERVER multitude FOREIGN DATA WRAPPER postgres_fdw OPTIONS (dbname '#{ENV["MULTITUDE_DBNAME"]}', host '#{ENV["MULTITUDE_DBHOST"]}');"
       execute "CREATE USER MAPPING for #{ENV["DB_USERNAME"]} SERVER multitude OPTIONS (user '#{ENV["MULTITUDE_DBUSER"]}', password '#{ENV["MULTITUDE_DBPASS"]}');"
     end
