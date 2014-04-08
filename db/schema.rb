f5383a0cb583d663a5baf34a2fdf56084ce440cc
Nícolas Iensen <nicolas@iensen.me>
2014-04-08 09:55:33 -0300

    Re create activities view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140408084725) do
+ActiveRecord::Schema.define(version: 20140408125151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
