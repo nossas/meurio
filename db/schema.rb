1bdcbb9db68e302c37b4ca7f88d2ac54d1ebdff0
Nícolas Iensen <nicolas@iensen.me>
2014-05-27 14:53:17 -0300

    [#71262542] add avatar to organization

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526180016) do
+ActiveRecord::Schema.define(version: 20140527151528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -229,6 +229,7 @@ ActiveRecord::Schema.define(version: 20140526180016) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "city"
+    t.string   "avatar"
   end
 
   create_table "mobilizations", force: true do |t|
