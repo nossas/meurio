41f30e68ac9e46cdee992c37e2e94a8ee5646c17
Josias Schneider <josias.ds@gmail.com>
2014-06-27 12:23:08 -0300

    [#71262424] add slug to Organization

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140528121157) do
+ActiveRecord::Schema.define(version: 20140627151823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -244,6 +244,7 @@ ActiveRecord::Schema.define(version: 20140528121157) do
     t.string   "city"
     t.string   "avatar"
     t.string   "name"
+    t.string   "slug"
   end
 
   create_table "rewards", force: true do |t|
