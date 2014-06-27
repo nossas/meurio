fd3771c74be4ec4dbeb2d89a20b2e6bec0878414
Josias Schneider <josias.ds@gmail.com>
2014-06-27 13:38:09 -0300

    [#71262574] add Cities section to home; fix header links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -84,7 +84,7 @@ $(function(){
     var target = this.hash, $target = $(target);
 
     $('html, body').stop().animate({
-      'scrollTop': $target.offset().top
+      'scrollTop': $target.prev().offset().top
     }, 1000, 'swing', function () {
       window.location.hash = target;
     });
