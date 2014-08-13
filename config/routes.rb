35d7c3cabd9dba3fdac66772cd2db4bfd0602920
Nícolas Iensen <nicolas@iensen.me>
2014-08-13 14:10:56 -0300

    Fix edit profile link in the Meu Rio UI header

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Meurio::Application.routes.draw do
 
   get 'team' => 'users#team'
   get 'funders' => 'users#funders'
-  get 'edit_profile' => 'users#edit', as: :edit_profile
+  # get 'edit_profile' => 'users#edit', as: :edit_profile
   get 'login' => redirect { |params, request| "#{ENV['ACCOUNTS_HOST']}/login?#{request.params.to_query}" }, as: :login
 
   get ENV['MEURIO_ACCOUNTS_URL'] => 'mobilizations#index' if Rails.env.test?
