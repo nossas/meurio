ef7d51d24a2406ab8d41a1ce684dc31ab3d1e275
Nícolas Iensen <nicolas@iensen.me>
2014-09-13 18:35:09 -0300

    [#75431692] Fix migration

--- db/migrate/20140913212653_add_server_compartilhaco.rb
+++ db/migrate/20140913212653_add_server_compartilhaco.rb
@@ -5,7 +5,7 @@ class AddServerCompartilhaco < ActiveRecord::Migration
       raise "COMPARTILHACO_DBHOST is missing" if ENV["COMPARTILHACO_DBHOST"].nil?
       raise "COMPARTILHACO_DBUSER is missing" if ENV["COMPARTILHACO_DBUSER"].nil?
       raise "COMPARTILHACO_DBPASS is missing" if ENV["COMPARTILHACO_DBPASS"].nil?
-      execute "CREATE EXTENSION postgres_fdw;"
+      execute "CREATE EXTENSION IF NOT EXISTS postgres_fdw;"
       execute "CREATE SERVER compartilhaco FOREIGN DATA WRAPPER postgres_fdw OPTIONS (dbname '#{ENV["COMPARTILHACO_DBNAME"]}', host '#{ENV["COMPARTILHACO_DBHOST"]}');"
       execute "CREATE USER MAPPING for #{ENV["DB_USERNAME"]} SERVER compartilhaco OPTIONS (user '#{ENV["COMPARTILHACO_DBUSER"]}', password '#{ENV["COMPARTILHACO_DBPASS"]}');"
     end
