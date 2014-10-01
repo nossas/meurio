11a759d9abc6cfc7694d21648adcd5296d705271
Nícolas Iensen <nicolas@iensen.me>
2014-10-01 17:05:12 -0300

    [#79691628] add slider, about and program content to the page#founders page

--- spec/features/view_founders_page_spec.rb
+++ spec/features/view_founders_page_spec.rb
@@ -5,4 +5,14 @@ feature "View founders page" do
     visit founders_path
     expect(page).to have_css(".founders-slider")
   end
+
+  it "should have an about section" do
+    visit founders_path
+    expect(page).to have_css(".founders-about")
+  end
+
+  it "should have an program section" do
+    visit founders_path
+    expect(page).to have_css(".founders-program")
+  end
 end
