c9de9ab6a34ad5636d569e0b9616ec94bc250306
Josias Schneider <josias.ds@gmail.com>
2014-10-02 14:34:46 -0300

    [#79812094] change locale pt-BR to pt

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Meurio
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = 'pt-BR'
+    config.i18n.default_locale = 'pt'
 
     config.sass.preferred_syntax = :sass
     config.generators do |g|
