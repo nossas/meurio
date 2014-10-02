bc54606964d9f342ff4daeafbbc638a1143fed7b
Josias Schneider <josias.ds@gmail.com>
2014-10-02 14:32:21 -0300

    [#79812094] set locale globally

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,4 @@
 class PagesController < ApplicationController
-  before_action :set_locale
- 
-  def set_locale
-    I18n.locale = params[:l] || I18n.default_locale
-  end
-
   def founders
   end
 end
