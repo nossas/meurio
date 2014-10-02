94aed73699f46a22d65ab339e7ba9906446f345e
Josias Schneider <josias.ds@gmail.com>
2014-10-02 14:33:49 -0300

    [#79812094] make locale path global and optional

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Meurio::Application.routes.draw do
-  get "organizations/show"
-  root 'mobilizations#index'
+  
+  scope "(:locale)", locale: /pt|en/ do
+    root 'mobilizations#index'
+    get 'organizations/show'
 
-  resources :mobilizations
-  resources :task_types, except: [:destroy]
-  resources :badges, except: [:show]
-  resources :organizations, only: [:show]
-  resources :users, only: [:index, :show]
-  resources :rewards, only: [:create]
+    resources :mobilizations
+    resources :task_types, except: [:destroy]
+    resources :badges, except: [:show]
+    resources :organizations, only: [:show]
+    resources :users, only: [:index, :show]
+    resources :rewards, only: [:create]
 
-  get 'team' => 'users#team'
-  get 'funders' => 'users#funders'
-  get 'founders' => 'pages#founders'
-  # get 'edit_profile' => 'users#edit', as: :edit_profile
-  get 'login' => redirect { |params, request| "#{ENV['ACCOUNTS_HOST']}/login?#{request.params.to_query}" }, as: :login
+    get 'team' => 'users#team'
+    get 'funders' => 'users#funders'
+    get 'founders' => 'pages#founders'
+    # get 'edit_profile' => 'users#edit', as: :edit_profile
+    get 'login' => redirect { |params, request| "#{ENV['ACCOUNTS_HOST']}/login?#{request.params.to_query}" }, as: :login
 
-  get ENV['MEURIO_ACCOUNTS_URL'] => 'mobilizations#index' if Rails.env.test?
+    get ENV['MEURIO_ACCOUNTS_URL'] => 'mobilizations#index' if Rails.env.test?
+  end
 end
