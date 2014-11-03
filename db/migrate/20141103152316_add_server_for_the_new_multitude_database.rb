9230591f69651ceb85b653192f7d334988efdedc
Nícolas Iensen <nicolas.iensen@gmail.com>
2014-11-03 14:09:17 -0200

    Fix migration

--- db/migrate/20141103152316_add_server_for_the_new_multitude_database.rb
+++ db/migrate/20141103152316_add_server_for_the_new_multitude_database.rb
@@ -5,8 +5,8 @@ class AddServerForTheNewMultitudeDatabase < ActiveRecord::Migration
       raise "MULTITUDE_DBHOST is missing" if ENV["MULTITUDE_DBHOST"].nil?
       raise "MULTITUDE_DBUSER is missing" if ENV["MULTITUDE_DBUSER"].nil?
       raise "MULTITUDE_DBPASS is missing" if ENV["MULTITUDE_DBPASS"].nil?
-      execute "ALTER SERVER multitude OPTIONS (SET dbname '#{ENV["MULTITUDE_DBNAME"]}', host '#{ENV["MULTITUDE_DBHOST"]}');"
-      execute "ALTER USER MAPPING FOR #{ENV["DB_USERNAME"]} SERVER multitude OPTIONS (SET user '#{ENV["MULTITUDE_DBUSER"]}', password '#{ENV["MULTITUDE_DBPASS"]}');"
+      execute "ALTER SERVER multitude OPTIONS (SET dbname '#{ENV["MULTITUDE_DBNAME"]}', SET host '#{ENV["MULTITUDE_DBHOST"]}');"
+      execute "ALTER USER MAPPING FOR #{ENV["DB_USERNAME"]} SERVER multitude OPTIONS (SET user '#{ENV["MULTITUDE_DBUSER"]}', SET password '#{ENV["MULTITUDE_DBPASS"]}');"
     end
   end
 
