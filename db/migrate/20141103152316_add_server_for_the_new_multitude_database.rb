31f248428e0eb82589332eae04595c08ab12898c
Nícolas Iensen <nicolas.iensen@gmail.com>
2014-11-03 13:28:52 -0200

    Add server for the new Multitude database

---
+++ db/migrate/20141103152316_add_server_for_the_new_multitude_database.rb
@@ -0,0 +1,15 @@
+class AddServerForTheNewMultitudeDatabase < ActiveRecord::Migration
+  def up
+    if Rails.env.production? || Rails.env.staging?
+      raise "MULTITUDE_DBNAME is missing" if ENV["MULTITUDE_DBNAME"].nil?
+      raise "MULTITUDE_DBHOST is missing" if ENV["MULTITUDE_DBHOST"].nil?
+      raise "MULTITUDE_DBUSER is missing" if ENV["MULTITUDE_DBUSER"].nil?
+      raise "MULTITUDE_DBPASS is missing" if ENV["MULTITUDE_DBPASS"].nil?
+      execute "ALTER SERVER multitude OPTIONS (dbname '#{ENV["MULTITUDE_DBNAME"]}', host '#{ENV["MULTITUDE_DBHOST"]}');"
+      execute "ALTER USER MAPPING FOR #{ENV["DB_USERNAME"]} SERVER multitude OPTIONS (user '#{ENV["MULTITUDE_DBUSER"]}', password '#{ENV["MULTITUDE_DBPASS"]}');"
+    end
+  end
+
+  def down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027180528) do
+ActiveRecord::Schema.define(version: 20141103152316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
