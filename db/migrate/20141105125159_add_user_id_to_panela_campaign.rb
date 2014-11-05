b32a7964a80cbf63cc8258b4235f709ecde72a60
Nícolas Iensen <nicolas.iensen@gmail.com>
2014-11-05 10:56:09 -0200

    [#81002296] fix migration

--- db/migrate/20141105125159_add_user_id_to_panela_campaign.rb
+++ db/migrate/20141105125159_add_user_id_to_panela_campaign.rb
@@ -1,5 +1,5 @@
 class AddUserIdToPanelaCampaign < ActiveRecord::Migration
   def change
-    add_column :panela_campaigns, :user_id, :integer
+    add_column :panela_campaigns, :user_id, :integer, foreign_key: false
   end
 end
