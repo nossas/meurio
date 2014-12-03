d5f77c3e908154106f5cfc0bd59a206046e84e47
Nícolas Iensen <nicolas.iensen@gmail.com>
2014-12-03 15:51:10 -0200

    [#83544614] fix RecentActivity model

--- app/models/recent_activity.rb
+++ app/models/recent_activity.rb
@@ -1,2 +1,5 @@
-class RecentActivity < Activity
+class RecentActivity < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :activable, polymorphic: true
+  has_one :mobilization, primary_key: :hashtag, foreign_key: :hashtag
 end
