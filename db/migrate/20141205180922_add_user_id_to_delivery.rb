e818b180316538105a62e63a10c8823aee8300d9
Nícolas Iensen <nicolas.iensen@gmail.com>
2014-12-05 16:31:58 -0200

    [#83704408] fix latest migrations

--- db/migrate/20141205180922_add_user_id_to_delivery.rb
+++ db/migrate/20141205180922_add_user_id_to_delivery.rb
@@ -1,5 +1,5 @@
 class AddUserIdToDelivery < ActiveRecord::Migration
   def change
-    add_column :deliveries, :user_id, :integer
+    add_column :deliveries, :user_id, :integer, foreign_key: false
   end
 end
--- db/migrate/20141205180938_add_task_id_to_delivery.rb
+++ db/migrate/20141205180938_add_task_id_to_delivery.rb
@@ -1,5 +1,5 @@
 class AddTaskIdToDelivery < ActiveRecord::Migration
   def change
-    add_column :deliveries, :task_id, :integer
+    add_column :deliveries, :task_id, :integer, foreign_key: false
   end
 end
