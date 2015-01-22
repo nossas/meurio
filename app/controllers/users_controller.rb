d053656f485fa7230f7b3eb6b83b2c992c5912c3
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-01-22 14:16:13 -0200

    [#84328358] limit succeed campaigns by 3 in the user's page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < InheritedResources::Base
     @matching_tasks = Task.matching(@user.skills)
     @subscribed_tasks = Task.subscribed(@user.id)
     @finished_tasks = Task.finished(@user.id)
-    @succeed_poked_panela_campaigns = @user.panela_campaigns.successful
+    @succeed_poked_panela_campaigns = @user.panela_campaigns.successful.limit(3)
 
     @subscribed_tasks = @subscribed_tasks - @finished_tasks
   end
