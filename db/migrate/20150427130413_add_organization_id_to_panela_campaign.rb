03313a23a2aad357e56d676e8f23e54803ed6a9d
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-04-27 10:08:43 -0300

    [#91968802] add organization_id to panela_campaigns

--- db/migrate/20150427130413_add_organization_id_to_panela_campaign.rb
+++ db/migrate/20150427130413_add_organization_id_to_panela_campaign.rb
@@ -1,5 +1,5 @@
 class AddOrganizationIdToPanelaCampaign < ActiveRecord::Migration
   def change
-    add_column :panela_campaigns, :organization_id, :integer
+    add_column :panela_campaigns, :organization_id, :integer, foreign_key: false
   end
 end
