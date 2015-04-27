5d2635dfdcffa12e345cd3d6d7b946ff750802eb
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-04-27 11:08:31 -0300

    [#91968802] added organization_id to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427134158) do
+ActiveRecord::Schema.define(version: 20150427140742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 20150427134158) do
     t.string   "address_district"
     t.string   "website"
     t.boolean  "sponsor",          default: false
+    t.integer  "organization_id"
   end
 
   create_table "deliveries", force: true do |t|
